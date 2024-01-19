4aeddd49aaf22934c7f8fae0fe99976d87aae69e
Pethum229 <pathumpp99@gmail.com>
2024-01-19 14:18:09 +0530

    Initial Commit

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -9,13 +9,33 @@ namespace Money_Management.Models
         public int TransactionId { get; set; }
 
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        public Category? Category { get; set; }
         public int Amount { get; set; }
 
         [Column(TypeName = "nvarchar(75)")]
         public string? Note { get; set; }
 
         public DateTime Date { get; set; } = DateTime.Now;
+
+        [NotMapped]
+        public string? CategoryTitleWithIcon
+        {
+            get
+            {
+                return Category == null ? "" : Category.Icon + " " + Category.Title;
+            }
+        
+        }
+
+        [NotMapped]
+        public string? FormattedAmount
+        {
+            get
+            {
+                return ((Category == null || Category.Type=="Expense" )? "- " : "+ ") + Amount.ToString("C0");
+            }
+
+        }
 
     }
 }
